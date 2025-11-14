e86aee1e8d07108fac63695ddb40cc2cc821af9a
ccavalieri <133131478+ccavalieri@users.noreply.github.com>
2025-11-14 10:36:51 -0300

    add path for colab running

--- fedsiam-labels-at-client/run_experiments.py
+++ fedsiam-labels-at-client/run_experiments.py
@@ -35,9 +35,9 @@ from pathlib import Path
 
 # Methods to run
 METHODS = {
-    'baseline': 'main_fedsiam_d.py',
-    'fa': 'train_fedsiam_d_with_feature_alignment.py',
-    'kd': 'train_fedsiam_d_with_knowledge_distillation.py'
+    'baseline': '/content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d.py',
+    'fa': '/content/fedsiam-pytorch/fedsiam-labels-at-client/train_fedsiam_d_with_feature_alignment.py',
+    'kd': '/content/fedsiam-pytorch/fedsiam-labels-at-client/train_fedsiam_d_with_knowledge_distillation.py'
 }
 
 # Datasets
