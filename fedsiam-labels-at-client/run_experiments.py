2ddaf698f180e46446caf7400695da24d26e58ee
ccavalieri <133131478+ccavalieri@users.noreply.github.com>
2025-11-17 18:42:26 -0300

    Update run_experiments.py

--- fedsiam-labels-at-client/run_experiments.py
+++ fedsiam-labels-at-client/run_experiments.py
@@ -26,10 +26,10 @@ from pathlib import Path
 
 # Methods to run 
 METHODS = {
-    'baseline': 'content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d.py',
-    'fa': 'content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d_with_feature_alignment.py',
-    'kd': 'content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d_with_knowledge_distillation.py',
-    'proto': 'content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d_with_prototype.py'
+    'baseline': '/content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d.py',
+    'fa': '/content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d_with_feature_alignment.py',
+    'kd': '/content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d_with_knowledge_distillation.py',
+    'proto': '/content/fedsiam-pytorch/fedsiam-labels-at-client/main_fedsiam_d_with_prototype.py'
 }
 
 # Datasets
